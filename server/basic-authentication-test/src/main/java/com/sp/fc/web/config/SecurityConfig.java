59a744a95c6473ae728f6b8f2e3e01a860ba4e3e
SeHoonBae <47914153+SeHoonBae@users.noreply.github.com>
2022-06-01 19:44:22 +0900

    BasicAuthentication 실습

---
+++ server/basic-authentication-test/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.sp.fc.web.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@EnableWebSecurity // 해당 Annotation이 없으면 WebSecurityConfig 작동 안함.
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable() // post방식은 csrf를 탐
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser(
+                        User.withDefaultPasswordEncoder()
+                        .username("user1")
+                        .password("1111")
+                        .roles("USER")
+                        .build()
+                );
+    }
+}
